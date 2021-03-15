05f117eaf755a3f9273015acf13d4ae92973cc3a
Ievgen Perederieiev <peredereev.ievgen@gmail.com>
2021-03-15 19:52:12 +0200

    added small set of tests for Utils.

--- Utils/tests/test_utils.py
+++ Utils/tests/test_utils.py
@@ -2,3 +2,14 @@ import pytest
 
 import Utils.utils as utils
 
+
+def test_random_name_file_length():
+    assert len(utils.generate_random_name()) == 20
+
+
+def test_random_name_format():
+    assert utils.generate_random_name().split('.')[1] == 'txt'
+
+
+def test_random_name_len():
+    assert len(utils.generate_random_name().split('.')[0]) == 16
